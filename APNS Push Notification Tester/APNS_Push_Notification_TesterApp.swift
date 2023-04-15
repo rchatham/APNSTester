fd9dc6c09c4cbb246abeb628af4c5541e187c5fe
Reid Chatham <reid.chatham@gmail.com>
2023-04-14 21:24:28 -0700

    Initial Commit

---
+++ APNS Push Notification Tester/APNS_Push_Notification_TesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  APNS_Push_Notification_TesterApp.swift
+//  APNS Push Notification Tester
+//
+//  Created by Reid Chatham on 4/14/23.
+//
+
+import SwiftUI
+
+@main
+struct APNS_Push_Notification_TesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
